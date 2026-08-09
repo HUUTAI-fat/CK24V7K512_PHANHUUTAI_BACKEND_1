95a1f9b47e1d7e85d345b867517c2f487da6e579
Phan Hữu Tài <154339663+HUUTAI-fat@users.noreply.github.com>
2026-08-09 12:02:27 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const contactRoutes = require("./app/routes/contact.route");
+
+const ApiError = require("./app/api-error");
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -10,5 +14,17 @@ app.use("/api/contacts", contactRoutes);
 app.get("/", (req, res) => {
 
   res.json({ message: "Welcome to contact book application." });
+});
+
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
 });
 module.exports = app;
