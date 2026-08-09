3c9cfe881955bac4bd89495af7fbd4936f2e6260
Phan Hữu Tài <154339663+HUUTAI-fat@users.noreply.github.com>
2026-08-09 11:47:07 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const contactRoutes = require("./app/routes/contact.route");
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/contacts", contactRoutes);
+
 app.get("/", (req, res) => {
+
   res.json({ message: "Welcome to contact book application." });
 });
 module.exports = app;
